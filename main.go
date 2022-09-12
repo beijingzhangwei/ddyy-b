6f2fcb5f787e1f631a6e0f419219fe693aa652ba
zhanwei <zhangwei@duduyixia.com>
2022-09-12 11:34:37 +0800

    init

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
-
-	fmt.Println("hello world!")
+	r := mux.NewRouter()
+	fs := http.FileServer(http.Dir("./dist"))
+	r.PathPrefix("/").Handler(fs)
+	http.Handle("/", r)
+	fmt.Println("Listening")
+	log.Panic(http.ListenAndServe(":3000", nil),
+	)
 }
